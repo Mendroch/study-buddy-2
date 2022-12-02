e52fcf72d7636e239122e06f4e968c2217a5684b
OPTIMAL IT <optimalit@Mac-mini-OPTIMAL.local>
2022-12-02 11:14:39 +0100

    forms-and-routing

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'components/atoms/Button/Button';
+import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import Average from 'components/atoms/Average/Average';
 import { StyledInfo, Wrapper } from './UsersListItem.styles';
 
@@ -10,7 +10,7 @@ const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%
     <StyledInfo>
       <p>
         {name}
-        <Button onClick={() => deleteUser(name)} />
+        <DeleteButton onClick={() => deleteUser(name)} />
       </p>
       <p>attendance: {attendance}</p>
     </StyledInfo>
